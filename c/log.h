d685a955d92299032a205fe62482f4ee946f669f
Matthew Malensek <matt@malensek.net>
2019-06-01 23:55:27 -0700

    Add log header

---
+++ c/log.h
@@ -0,0 +1,16 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+#define LOG(fmt, ...) \
+        do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+
+#define LOGP(str) \
+    do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): %s", __FILE__, \
+            __LINE__, __func__, str); } while (0)
+
+#endif
