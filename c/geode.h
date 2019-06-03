80506aa142a9b65becfe4bfceeca58575a381a9e
Matthew Malensek <matt@malensek.net>
2019-06-02 22:52:00 -0700

    Add header

---
+++ c/geode.h
@@ -0,0 +1,38 @@
+#ifndef _GEODE_H_
+#define _GEODE_H_
+
+#include "geohash.h"
+#include "roaring.h"
+#include "uthash.h"
+
+#define PREFIX_SZ 2
+
+struct geode {
+    char prefix[PREFIX_SZ + 1];
+    GeoCoord base_geohash;
+    unsigned int width;
+    unsigned int height;
+    double x_deg;
+    double y_deg;
+    double x_px;
+    double y_px;
+    roaring_bitmap_t *bmp;
+    UT_hash_handle hh;
+};
+
+/**
+ * Initializes a new GEODE data structure using the provided base Geohash.
+ *
+ * This function allocates memory; the resulting GEODE struct should be freed
+ * using geode_free().
+ *
+ * @param base_geohash Geohash to use as the bounding box for this GEODE, e.g.,
+ * a base Geohash of 'dj' creates a new GEODE that is centered on the southern
+ * USA near Alabama, Georgia, North Florida.
+ *
+ * @return The initialized GEODE.
+ */
+struct geode *geode_create(char *base_geohash, unsigned int precision);
+void geode_free(struct geode *g);
+
+#endif
