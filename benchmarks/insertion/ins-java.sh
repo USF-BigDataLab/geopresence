ae32901d95ddb7b254bde6d153759cdf594c4927
Matthew Malensek <matt@malensek.net>
2019-06-06 16:58:53 +0000

    Add java insertion benchmark script

---
+++ benchmarks/insertion/ins-java.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+out_dir="$(pwd)/java_out"
+rm -rf "${out_dir}"
+mkdir -vp "${out_dir}"
+
+export JAVA_HOME="${HOME}/jdk1.8.0_212"
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+java -version
+
+cd ../../java
+pwd
+
+mvn clean package
+
+for i in {1..100}; do
+	echo -n "${i} ... "
+	/usr/bin/time -v java \
+		-Xmx800M -Xms800M \
+		-cp ./target/geogrid-1.0.jar \
+			geogrid.Driver \
+			&> "${out_dir}/run.${i}.txt"
+	echo "X"
+done
