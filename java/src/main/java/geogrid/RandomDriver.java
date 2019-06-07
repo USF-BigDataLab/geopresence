193d1fe37a4e3cf09f27dfc0759f0e248fd6f2cb
Matthew Malensek <matt@malensek.net>
2019-06-07 20:52:49 +0000

    Streamline loop

--- java/src/main/java/geogrid/RandomDriver.java
+++ java/src/main/java/geogrid/RandomDriver.java
@@ -5,25 +5,22 @@ public class RandomDriver {
     public void benchmark(long iterations) {
         long startTime = System.nanoTime();
 
-        GeoavailabilityGrid gg = new GeoavailabilityGrid("9x", 16);
+        GeoavailabilityGrid gg = new GeoavailabilityGrid("9x", 20);
+
+        float minLat = gg.getBaseRange().getLowerBoundForLatitude();
+        float maxLat = gg.getBaseRange().getUpperBoundForLatitude();
+        float minLon = gg.getBaseRange().getLowerBoundForLongitude();
+        float maxLon = gg.getBaseRange().getUpperBoundForLongitude();
 
         for (int i = 0; i < iterations; ++i) {
-            float minLat = gg.getBaseRange().getLowerBoundForLatitude();
-            float maxLat = gg.getBaseRange().getUpperBoundForLatitude();
             float randomLat = minLat + (float) Math.random() * (maxLat - minLat);
-
-            float minLon = gg.getBaseRange().getLowerBoundForLongitude();
-            float maxLon = gg.getBaseRange().getUpperBoundForLongitude();
             float randomLon = minLon + (float) Math.random() * (maxLon - minLon);
 
             gg.addPoint(new Coordinates(randomLat, randomLon));
         }
 
-        Bitmap b = gg.getBitmap();
-
         long endTime = System.nanoTime();
         System.out.println((endTime - startTime) * 1E-6);
-        System.out.println(b.cardinality());
     }
 
     public static void main(String[] args) throws Exception {
