18b289ab708571e7ceed39ff2e7fb9a13c84f2ad
Matthew Malensek <matt@malensek.net>
2019-06-07 20:49:17 +0000

    Add 2x insertion scripts

---
+++ benchmarks/insertion/ins-c-2x.sh
@@ -0,0 +1,24 @@
+#/usr/bin/env bash
+
+out_dir="$(pwd)/c_2x_out"
+rm -rf "${out_dir}"
+mkdir -vp "${out_dir}"
+
+cd ../../c
+pwd
+
+echo -n "Compiling... "
+gcc -O3 bench_ins.c geode.c geohash.c timer.c -o bench_ins
+echo "Done"
+
+for i in {1..100}; do
+    echo -n "${i} ... "
+    /usr/bin/time -v ./bench_ins \
+        ../datasets/double_geohashes.txt &> "${out_dir}/run.${i}.txt"
+    echo "X"
+done
+
+grep 'wall clock' "${out_dir}"/run.*.txt | awk '{print $9}' | sed 's|0:||g' \
+    > "${out_dir}/wall_clock.txt"
+
+txtstats "${out_dir}/wall_clock.txt"
---
+++ benchmarks/insertion/ins-java-2x.sh
@@ -0,0 +1,30 @@
+#/usr/bin/env bash
+
+out_dir="$(pwd)/java_2x_out"
+rm -rf "${out_dir}"
+mkdir -vp "${out_dir}"
+
+export JAVA_HOME="${HOME}/jdk1.8.0_212"
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+java -version
+
+cd ../../java
+pwd
+
+mvn clean package
+
+for i in {1..100}; do
+    echo -n "${i} ... "
+    /usr/bin/time -v java \
+        -Xmx800M -Xms800M \
+        -cp ./target/geogrid-1.0.jar \
+            geogrid.Driver ../datasets/double_geohashes.txt\
+                &> "${out_dir}/run.${i}.txt"
+    echo "X"
+done
+
+grep 'wall clock' "${out_dir}"/run.*.txt | awk '{print $9}' | sed 's|0:||g' \
+    > "${out_dir}/wall_clock.txt"
+
+txtstats "${out_dir}/wall_clock.txt"
