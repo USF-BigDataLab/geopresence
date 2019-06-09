c20ece8cc82357bff1345bb31e02ca382c699556
Matthew Malensek <matt@malensek.net>
2019-06-09 06:06:50 +0000

    Add random benchmark scripts

---
+++ benchmarks/insertion/ins-c-r.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+insertions=100000
+
+out_dir="$(pwd)/c_r_out"
+rm -rf "${out_dir}"
+mkdir -vp "${out_dir}"
+
+cd ../../c
+pwd
+
+echo -n "Compiling... "
+gcc -O3 bench_rand_ins.c geode.c geohash.c timer.c -o bench_rand_ins
+echo "Done"
+
+for i in {1..100}; do
+    echo -n "${i} ... "
+    /usr/bin/time -v ./bench_rand_ins ${insertions} &> "${out_dir}/run.${i}.txt"
+    echo "X"
+done
+
+grep 'wall clock' "${out_dir}"/run.*.txt | awk '{print $9}' | sed 's|0:||g' \
+    > "${out_dir}/wall_clock.txt"
+
+txtstats "${out_dir}/wall_clock.txt"
---
+++ benchmarks/insertion/ins-java-r.sh
@@ -0,0 +1,32 @@
+#/usr/bin/env bash
+
+insertions=100000
+
+out_dir="$(pwd)/java_r_out"
+rm -rf "${out_dir}"
+mkdir -vp "${out_dir}"
+
+export JAVA_HOME="${HOME}/jdk1.8.0_212"
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+java -version
+
+cd ../../java
+pwd
+
+mvn clean package
+
+for i in {1..100}; do
+    echo -n "${i} ... "
+    /usr/bin/time -v java \
+        -Xmx800M -Xms800M \
+        -cp ./target/geogrid-1.0.jar \
+            geogrid.RandomDriver ${insertions} \
+                &> "${out_dir}/run.${i}.txt"
+    echo "X"
+done
+
+grep 'wall clock' "${out_dir}"/run.*.txt | awk '{print $9}' | sed 's|0:||g' \
+    > "${out_dir}/wall_clock.txt"
+
+txtstats "${out_dir}/wall_clock.txt"
