432f1e8d3dfb66de5692588213efd773d0fbb305
Jason McGowan <jasonmcgowan357@gmail.com>
2023-05-18 16:58:35 -0700

    Implement control port as chord port +1

--- go/server/server.go
+++ go/server/server.go
@@ -6,8 +6,11 @@ import (
 	"hash"
 	"io"
 	"log"
+	"math"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"time"
 
 	"geopresence/chord"
@@ -19,7 +22,6 @@ import (
 type ServerCfg struct {
 	GeohashUpperBound string // Also chord node id
 	GeohashLowerBound string // Geohashes added below this are ignored
-	ControlHostname   string // Address to listen for system control messages
 	ChordHostname     string // Local host name and listen port
 	CreateNewChord    bool   // True to create a new ring
 	SisterId          string // Existing node Id
@@ -32,7 +34,6 @@ func configJson() string {
 	sc := &ServerCfg{
 		GeohashUpperBound: "789bcdef",
 		GeohashLowerBound: "6789bcde",
-		ControlHostname:   "10.0.0.1:12000",
 		ChordHostname:     "10.0.0.1:12345",
 		CreateNewChord:    false,
 		SisterId:          "ab123",
@@ -63,6 +64,26 @@ func Start(args []string) error {
 	return runGeoserver(sc)
 }
 
+func chordNameToControl(chn string) (controlName string, err error) {
+	splits := strings.SplitAfter(chn, ":")
+	if len(splits) != 2 {
+		return "", fmt.Errorf("chord hostname is invalid: %s", chn)
+	}
+	domain := splits[0]
+	chordPortStr := splits[1]
+	chordPort, err := strconv.ParseUint(chordPortStr, 10, 16)
+	if err != nil {
+		return "", fmt.Errorf("chord hostname %s has error parsing port: %w", chn, err)
+	}
+	// Check for -1 to avoid overflow to 0 on subsequent +1
+	if chordPort > math.MaxUint16-1 {
+		return "", fmt.Errorf("chord hostname %s has port too large", chn)
+	}
+	controlPort := chordPort + 1
+	controlPortStr := strconv.FormatUint(controlPort, 10)
+	return domain + controlPortStr, nil
+}
+
 func runGeoserver(sc *ServerCfg) error {
 	cnode, err := startChord(sc)
 	if err != nil {
@@ -70,9 +91,14 @@ func runGeoserver(sc *ServerCfg) error {
 	}
 	defer cnode.Stop()
 
+	controlName, err := chordNameToControl(sc.ChordHostname)
+	if err != nil {
+		return err
+	}
+
 	msgChan := make(chan *messages.MessageHandler)
 	defer close(msgChan)
-	connHandler := NewConnHandler(sc.ControlHostname, msgChan)
+	connHandler := NewConnHandler(controlName, msgChan)
 	err = connHandler.Open()
 	if err != nil {
 		return err
@@ -117,7 +143,8 @@ func handleMessage(mh *messages.MessageHandler, cnode *chord.Node, gr *geode.Geo
 	}
 }
 
-func handleAddGeohashRequest(msg *messages.AddGeohashRequest, mh *messages.MessageHandler, cnode *chord.Node, gr *geode.GeoRange) {
+func handleAddGeohashRequest(msg *messages.AddGeohashRequest, mh *messages.MessageHandler,
+	cnode *chord.Node, gr *geode.GeoRange) {
 	defer mh.Close()
 	for _, geohash := range msg.GetGeohashes() {
 		isInRange, err := gr.TryAddGeohash(geohash)
@@ -127,10 +154,15 @@ func handleAddGeohashRequest(msg *messages.AddGeohashRequest, mh *messages.Messa
 		// Valid geohash owned by another node
 		tgtNode, err := cnode.Find(geohash)
 		if err != nil {
-			log.Printf("error finding target for: %s, %s", geohash, err)
+			log.Printf("error finding target for: %s, %s", geohash, err.Error())
 			continue
 		}
-		err = messages.SendAddHashRequest(tgtNode.GetAddr(), []string{geohash})
+		controlName, err := chordNameToControl(tgtNode.GetAddr())
+		if err != nil {
+			log.Printf("error converting chord to control name: %s", err.Error())
+			continue
+		}
+		err = messages.SendAddHashRequest(controlName, []string{geohash})
 		if err != nil {
 			log.Printf("error forwarding hash request: %s", err)
 			continue
